1d369c18b9f5ea6bf510286b6a5928c5057545b9
Russell Gordon <rgordon@lcs.on.ca>
2021-08-02 14:51:56 -0400

    Basic list of messages now shows.

--- DataTaskChallenge/Views/ContentView.swift
+++ DataTaskChallenge/Views/ContentView.swift
@@ -12,30 +12,58 @@ struct ContentView: View {
     @State private var user: User = User(id: UUID(), name: "Patrick Stewart", age: 81)
     @State private var messages: [Message] = []
     @State private var favourites: Favourites = []
-
-
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .task {
-                do {
-                    
-                    let userURL = URL(string: "https://www.hackingwithswift.com/samples/user-24601.json")!
-                    let messagesURL = URL(string: "https://www.hackingwithswift.com/samples/user-messages.json")!
-                    let favouritesURL = URL(string: "https://www.hackingwithswift.com/samples/user-favorites.json")!
-
-                    async let userData: User = URLSession.shared.decode(from: userURL)
-                    async let messagesData: [Message] = URLSession.shared.decode(from: messagesURL)
-                    async let favouritesData: Favourites = URLSession.shared.decode(from: favouritesURL)
-                    
-                    user = try await userData
-                    messages = try await messagesData
-                    favourites = try await favouritesData
-                    
-                } catch {
-                    print(error.localizedDescription)
+        TabView {
+            
+            NavigationView {
+                VStack {
+                    Text("**Age:** \(user.age)")
                 }
+                .navigationTitle(user.name)
             }
+            .tabItem {
+                Image(systemName: "person")
+                Text("User")
+            }
+            
+            NavigationView {
+                List(messages) { message in
+                    VStack(alignment: .leading) {
+                        Text(message.message)
+                        Text(message.from)
+                            .font(.caption)
+                            .bold()
+                    }
+                }
+                .navigationTitle(user.name)
+            }
+            .tabItem {
+                Image(systemName: "message.fill")
+                Text("Messages")
+            }
+            
+        }
+        .task {
+            do {
+                
+                let userURL = URL(string: "https://www.hackingwithswift.com/samples/user-24601.json")!
+                let messagesURL = URL(string: "https://www.hackingwithswift.com/samples/user-messages.json")!
+                let favouritesURL = URL(string: "https://www.hackingwithswift.com/samples/user-favorites.json")!
+                
+                async let userData: User = URLSession.shared.decode(from: userURL)
+                async let messagesData: [Message] = URLSession.shared.decode(from: messagesURL)
+                async let favouritesData: Favourites = URLSession.shared.decode(from: favouritesURL)
+                
+                user = try await userData
+                messages = try await messagesData
+                print("There are \(messages.count) messages.")
+                favourites = try await favouritesData
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
     
 }
