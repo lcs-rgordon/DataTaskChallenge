424bcf3ba33a606db1fd1f107d08162219f3a82f
Russell Gordon <rgordon@lcs.on.ca>
2021-08-02 14:23:11 -0400

    Initial Commit

---
+++ DataTaskChallenge/DataTaskChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataTaskChallengeApp.swift
+//  DataTaskChallenge
+//
+//  Created by Russell Gordon on 2021-08-02.
+//
+
+import SwiftUI
+
+@main
+struct DataTaskChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
